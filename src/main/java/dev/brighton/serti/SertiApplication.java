616c7eb975749bd6f40eeb204e52670a19817f17
Carlos Brighton <cbrghton@gmail.com>
2025-04-17 13:09:00 -0600

    Primer Commit

---
+++ src/main/java/dev/brighton/serti/SertiApplication.java
@@ -0,0 +1,13 @@
+package dev.brighton.serti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SertiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SertiApplication.class, args);
+	}
+
+}
