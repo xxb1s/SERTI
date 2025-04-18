5d240256a02ef5f113e62e40d34ab7bc632ed416
Carlos Brighton <cbrghton@gmail.com>
2025-04-18 17:24:59 -0600

    Establece la configuración para la conexión con la base de datos usando variables de entorno

--- src/main/java/dev/brighton/serti/SertiApplication.java
+++ src/main/java/dev/brighton/serti/SertiApplication.java
@@ -2,12 +2,31 @@ package dev.brighton.serti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class SertiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SertiApplication.class, args);
+		Dotenv dotenv = Dotenv.load();
+
+		String host = dotenv.get("DB_HOST");
+		String port = dotenv.get("DB_PORT");
+		String database = dotenv.get("DB_DATABASE");
+
+		String url = String.format("jdbc:mysql://%s:%s/%s", host, port, database);
+
+		Map<String, Object> props = new HashMap<>();
+		props.put("spring.application.name", dotenv.get("APP_NAME"));
+		props.put("spring.datasource.url", url);
+		props.put("spring.datasource.username", dotenv.get("DB_USER"));
+		props.put("spring.datasource.password", dotenv.get("DB_PASSWORD"));
+
+		SpringApplication app = new SpringApplication(SertiApplication.class);
+		app.setDefaultProperties(props);
+		app.run(args);
 	}
 
 }
