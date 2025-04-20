0c7cc7836bce6a882377458496ebc0eaf140f84e
Carlos Brighton <cbrghton@gmail.com>
2025-04-19 19:17:50 -0600

    Agrega servicio para consumir la API de Pokemon

---
+++ src/main/java/dev/brighton/serti/services/PokemonAPIService.java
@@ -0,0 +1,41 @@
+package dev.brighton.serti.services;
+
+import dev.brighton.serti.dtos.PokemonDTO;
+import dev.brighton.serti.dtos.TypeDTO;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
+
+import java.util.Optional;
+
+@Service
+public class PokemonAPIService {
+    private final WebClient webClient;
+
+    public PokemonAPIService() {
+        Dotenv dotenv = Dotenv.load();
+
+        this.webClient = WebClient.builder()
+                .baseUrl(dotenv.get("POKEMON_API_URL"))
+                .build();
+    }
+
+    public Optional<PokemonDTO> getPokemonByPokedex(Long pokedex) {
+        try {
+            PokemonDTO pokemonDTO = webClient.get()
+                    .uri(String.format("/pokemon/%d", pokedex))
+                    .retrieve()
+                    .onStatus(status -> status.is4xxClientError(), response ->
+                        response.createException().flatMap(Mono::error)
+                    )
+                    .bodyToMono(PokemonDTO.class)
+                    .block();
+
+            return Optional.ofNullable(pokemonDTO);
+        } catch (WebClientResponseException e) {
+            return Optional.empty();
+        }
+    }
+}
